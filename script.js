a7c77bec794e926a5ec7548b9fa90ace5301fca8
DevQueen <124140753+oyingrace@users.noreply.github.com>
2023-10-19 22:31:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,21 +5,25 @@ document.getElementById("btn1").addEventListener("click", function(){
 });
 
 
-click_to_convert.addEventListener('click',function(){
-    var speech = true;
-    window.SpeechRecognition = window.webkitSpeechRecognition;
-    const recognition = new SpeechRecognition();
-    recognition.interimResults = true;
 
-    recognition.addEventListener('result', e=>{
-        const transcript = Array.from(e.results)
-        .map(result => result[0])
-        .map(result => result.transcript)
+ var startButton = document.getElementById('start');
+ var stopButton = document.getElementById('stop');
+ var resultElement = document.getElementById('convert_text');
 
-        convert_text.innerHTML = transcript;
-    })
+ var recognition = new webkitSpeechRecognition();
 
-    if(speech == true){
-        recognition.start();
-    }
-})
+ recognition.lang = window.navigator.language;
+ recognition.interimResults = true;
+
+ startButton.addEventListener('click',()=>{
+    recognition.start();
+ });
+
+ stopButton.addEventListener('click',()=>{
+    recognition.stop();
+ });
+
+ recognition.addEventListener('convert_text', (event) =>{
+    const convert_text = event.results[event.results.length - 1][0].transcript;
+    resultElement.textContent = convert_text;
+ });
