eb1e82fa604802c3bfad88229dd9b8cc621fd180
DevQueen <124140753+oyingrace@users.noreply.github.com>
2023-10-20 23:34:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,65 +1,111 @@
-//for the copy button
-document.getElementById("btn1").addEventListener("click", function(){
-    var copyText = document.getElementById("convert_text");
-    copyText.select();
-    document.execCommand("copy");
+const recordBtn = document.querySelector(".record"),
+  result = document.querySelector(".result"),
+  downloadBtn = document.querySelector(".download"),
+  inputLanguage = document.querySelector("#language"),
+  clearBtn = document.querySelector(".clear");
 
+let SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition,
+  recognition,
+  recording = false;
 
-var button = document.getElementById("btn1");
-button.innerHTML = "copied!";
-button.classList.add("copied");
+function populateLanguages() {
+  languages.forEach((lang) => {
+    const option = document.createElement("option");
+    option.value = lang.code;
+    option.innerHTML = lang.name;
+    inputLanguage.appendChild(option);
+  });
+}
 
-setTimeout(function(){
-    button.innerHTML = "Copy";
-    button.classList.remove("copied");
-}, 2000);
+populateLanguages();
 
+function speechToText() {
+  try {
+    recognition = new SpeechRecognition();
+    recognition.lang = inputLanguage.value;
+    recognition.interimResults = true;
+    recordBtn.classList.add("recording");
+    recordBtn.querySelector("p").innerHTML = "Listening...";
+    recognition.start();
+    recognition.onresult = (event) => {
+      const speechResult = event.results[0][0].transcript;
+      //detect when intrim results
+      if (event.results[0].isFinal) {
+        result.innerHTML += " " + speechResult;
+        result.querySelector("p").remove();
+      } else {
+        //creative p with class interim if not already there
+        if (!document.querySelector(".interim")) {
+          const interim = document.createElement("p");
+          interim.classList.add("interim");
+          result.appendChild(interim);
+        }
+        //update the interim p with the speech result
+        document.querySelector(".interim").innerHTML = " " + speechResult;
+      }
+      downloadBtn.disabled = false;
+    };
+    recognition.onspeechend = () => {
+      speechToText();
+    };
+    recognition.onerror = (event) => {
+      stopRecording();
+      if (event.error === "no-speech") {
+        alert("No speech was detected. Stopping...");
+      } else if (event.error === "audio-capture") {
+        alert(
+          "No microphone was found. Ensure that a microphone is installed."
+        );
+      } else if (event.error === "not-allowed") {
+        alert("Permission to use microphone is blocked.");
+      } else if (event.error === "aborted") {
+        alert("Listening Stopped.");
+      } else {
+        alert("Error occurred in recognition: " + event.error);
+      }
+    };
+  } catch (error) {
+    recording = false;
+
+    console.log(error);
+  }
+}
+
+recordBtn.addEventListener("click", () => {
+  if (!recording) {
+    speechToText();
+    recording = true;
+  } else {
+    stopRecording();
+  }
 });
 
-
-var  speechRecognition = window.webkitSpeechRecognition;
-
-var recognition = new speechRecognition()
- 
-var convert_text = $("convert_text");
-
-var instructions = $("instructions");
-
-var content = ""
-
-recognition.continuous = true;
-
-//recognition is started
-
-recognition.onstart = function (){
-    instructions.text("Speak, I'm Listening")
+function stopRecording() {
+  recognition.stop();
+  recordBtn.querySelector("p").innerHTML = "Start Listening";
+  recordBtn.classList.remove("recording");
+  recording = false;
 }
 
-recognition.onspeechend = function(){
-    instructions.text("Not speaking")
-}
 
-recognition.onerror = function(){
-    instructions.text("Try Again")
-}
-
-recognition.onresult = function(event){
-var current = event.resultIndex;
-
-var transcript = event.results[current][0].transcript;
-
-content += transcript
-
-convert_text.val(content)
-}
-
-$("#start").click(function (event){
-    if(content.length){
-        content += ""
-    }
-    recognition.start()
+clearBtn.addEventListener("click", () => {
+  result.innerHTML = "";
+  downloadBtn.disabled = true;
 });
 
-convert_text.on('input', function (){
-    content = $(this).val()
-});
+document.addEventListener("DOMContentLoaded", function(){
+    const copyButton = document.getElementById("copyBtn");
+    const copyText = document.getElementById("textArea");
+    const copyFeedback = document.getElementById("copyFdback");
+
+    copyButton.addEventListener("click", function(){
+        copyText.select();
+        document.execCommand("copy");
+
+        copyFeedback.textContent = "Copied";
+        setTimeout(function(){
+            copyFeedback.textContent = "";
+        }, 2000);
+        });
+      });
